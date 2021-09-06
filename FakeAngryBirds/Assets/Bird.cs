31336afd06f97e977a5c29c70d5539de35c0f3e6
Benasi <Benjaminmusic98@gmail.com>
2021-09-06 08:36:22 +0200

    FAT-0008: creating the slingshot methot to launch the bird [part1] the bird has gravity and flys in a bow formation

--- FakeAngryBirds/Assets/Bird.cs
+++ FakeAngryBirds/Assets/Bird.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    //private variable only acessed in this Bird class
+    Vector3 _initialPosition;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
     // gets called evry time we click on our bird
     private void OnMouseDown()
     {
@@ -14,12 +22,17 @@ public class Bird : MonoBehaviour
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
     //ment for draging the bird to sling shot it
     private void OnMouseDrag()
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(newPosition.x, newPosition.y);
+        transform.position = new Vector3(newPosition.x, newPosition.y );
     }
 }
