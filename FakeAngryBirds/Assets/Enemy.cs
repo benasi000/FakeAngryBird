52897ae8895ad7a227cee4177d00b88eea83538b
Benasi <Benjaminmusic98@gmail.com>
2021-09-07 16:02:20 +0200

    FAB-0019: FAB-0018: adding particles to the game, when an enemy is destroied, some clound are shown [part 2]

--- FakeAngryBirds/Assets/Enemy.cs
+++ FakeAngryBirds/Assets/Enemy.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Bird bird = collision.collider.GetComponent<Bird>();
         if (bird != null)
         {
+            Instantiate(_cloudParticlePrefab,transform.position, Quaternion.identity);
+
             Destroy(gameObject);
             return;
         }
@@ -21,6 +25,7 @@ public class Enemy : MonoBehaviour
 
         if(collision.contacts[0].normal.y < - 0.5)
         {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
