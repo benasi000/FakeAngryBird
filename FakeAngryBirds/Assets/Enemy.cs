cfda34bf98aa8cd7433ed913546e19e3ff2b94e7
Benasi <Benjaminmusic98@gmail.com>
2021-09-07 15:32:22 +0200

    FAB-0016: enemys wanish away when hit by the bird

---
+++ FakeAngryBirds/Assets/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+
+        if (bird != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
